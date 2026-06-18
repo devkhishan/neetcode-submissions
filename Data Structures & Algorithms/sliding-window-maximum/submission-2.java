2d08bd5f0c55482dd75c6da7bcec2cc7f1b48ece
varico <141219203+devkhishan@users.noreply.github.com>
2026-06-18 16:14:27 +0530

    Add: sliding-window-maximum - submission-2

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-2.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        Deque<Integer> que = new LinkedList<>();
+        int n = nums.length;
+
+        int[] res = new int[n-k+1];
+
+        int l = 0;
+
+        for(int r=0;r<n;r++) {
+            while (!que.isEmpty() && nums[r] > nums[que.getLast()]) que.removeLast();
+            que.addLast(r);
+
+            if (l > que.getFirst()) que.removeFirst();
+
+            if ((r+1) >= k) res[l++] = nums[que.getFirst()];
+              
+        }
+        return res;
+    }
+}
