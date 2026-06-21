4ef3f4ca8b572578334d3eb7b09d8ccd82c2964a
varico <141219203+devkhishan@users.noreply.github.com>
2026-06-21 07:46:27 +0530

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.java
@@ -0,0 +1,19 @@
+class Solution {
+    record Pair(int val, int ind) {}
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] res = new int[n];
+        int ind = 0;
+        Stack<Pair> stack = new Stack<>(); 
+        
+        for(int i=0;i<n;i++) {
+            while (!stack.isEmpty() && temperatures[i] > (stack.peek()).val()) {
+                Pair top = stack.pop();
+                res[top.ind()] = i - top.ind();
+            }
+            stack.push(new Pair(temperatures[i], i));
+        }
+
+        return res;
+    }
+}
