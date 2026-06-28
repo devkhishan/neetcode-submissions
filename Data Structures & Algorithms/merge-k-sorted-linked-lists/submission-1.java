3cec291ced85b0a5f3c86e18e9af10e9e21c0f07
varico <141219203+devkhishan@users.noreply.github.com>
2026-06-28 19:47:44 +0530

    Add: merge-k-sorted-linked-lists - submission-1

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-1.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public ListNode mergeList(ListNode l1, ListNode l2) {
+        ListNode res = new ListNode();
+        ListNode head = res; 
+
+        while (l1!=null && l2!=null) {
+            if (l1.val <= l2.val) {
+                head.next = l1;
+                l1 = l1.next;
+            } else {
+                head.next = l2;
+                l2 = l2.next;
+            }
+            head = head.next;
+        }
+        head.next = (l1 != null) ? l1 : l2;
+        return res.next;
+    }
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) return null;
+        while (lists.length > 1) {
+            List<ListNode> mergedLists = new ArrayList<>(); 
+
+            for(int i=0;i<lists.length;i+=2) {
+                ListNode l1 = lists[i]; 
+                ListNode l2 = ((i+1)<lists.length) ? lists[i+1] : null;
+                mergedLists.add(mergeList(l1,l2));
+            }
+            lists = mergedLists.toArray(new ListNode[0]);
+        }
+
+        return lists[0];
+    }
+}
