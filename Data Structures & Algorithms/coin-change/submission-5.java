49a3e2b3126d7a99845dfd107c2da21b2d77c484
varico <141219203+devkhishan@users.noreply.github.com>
2026-08-03 15:33:50 +0530

    Add: coin-change - submission-5

---
+++ Data Structures & Algorithms/coin-change/submission-5.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int n = coins.length;
+        int[][] dp = new int[n][amount+1];
+        
+        for(int t = 0; t <= amount; t++) {
+            if (t % coins[0] == 0) {
+                dp[0][t] = t / coins[0]; 
+            } else {
+                dp[0][t] = (int)1e9;
+            }
+        }
+
+        for(int i = 1; i < n; i++) {
+            for (int t = 0; t <= amount; t++) {
+                int notTake = dp[i-1][t];
+
+                int take = Integer.MAX_VALUE;
+                if ((t-coins[i]) >= 0) {
+                    take = 1 + dp[i][t-coins[i]];
+                }
+
+                dp[i][t] = Math.min(take, notTake);
+            }
+        }
+
+        return dp[n-1][amount] >= (int)1e9 ? -1 : dp[n-1][amount];
+    }
+}
